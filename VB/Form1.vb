cfd5400822ad3cb29f3efdd9f77b9de04fbc2294
DevExpressExampleBot <fakeEmail>
2023-12-13 16:43:07 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,22 +1,23 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.DataAccess.Sql
 Imports DevExpress.XtraReports.UI
+
 ' ...
-
 Namespace docCustomDataItemsNames
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			Dim report As New XtraReport1()
-			report.DataSource = New MySqlDataSource(CType(report.DataSource, SqlDataSource))
-			Dim designTool As New ReportDesignTool(report)
-			designTool.ShowDesignerDialog()
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim report As XtraReport1 = New XtraReport1()
+            report.DataSource = New MySqlDataSource(CType(report.DataSource, SqlDataSource))
+            Dim designTool As ReportDesignTool = New ReportDesignTool(report)
+            designTool.ShowDesignerDialog()
+        End Sub
+    End Class
 End Namespace
--- VB/MySqlDataSource.vb
+++ VB/MySqlDataSource.vb
@@ -1,67 +1,67 @@
-﻿#Region "#Usings"
-Imports System
+'#Region "#Usings"
 Imports DevExpress.Data
 Imports DevExpress.DataAccess.Sql
-' ...
-#End Region ' #Usings
 
+' ...
+'#End Region  ' #Usings
 Namespace docCustomDataItemsNames
 
-	#Region "#SqlDataSource"
-	Public Class MySqlDataSource
-		Inherits SqlDataSource
-		Implements IDisplayNameProvider
+'#Region "#SqlDataSource"
+    Public Class MySqlDataSource
+        Inherits SqlDataSource
+        Implements IDisplayNameProvider
 
-		Public Sub New()
-		End Sub
+        Public Sub New()
+        End Sub
 
-		Public Sub New(ByVal copyFrom As SqlDataSource)
-			Me.LoadFromXml(copyFrom.SaveToXml())
-		End Sub
+        Public Sub New(ByVal copyFrom As SqlDataSource)
+            LoadFromXml(copyFrom.SaveToXml())
+        End Sub
 
-		' ...
-		Private Function IDisplayNameProvider_GetDataSourceDisplayName() As String Implements IDisplayNameProvider.GetDataSourceDisplayName
-			' Substitute the default datasource display name
-			' with a custom one.
-			Return "Northwind Traders"
-		End Function
+        ' ...
+        Private Function GetDataSourceDisplayName() As String Implements IDisplayNameProvider.GetDataSourceDisplayName
+            ' Substitute the default datasource display name
+            ' with a custom one.
+            Return "Northwind Traders"
+        End Function
 
-		Private Function IDisplayNameProvider_GetFieldDisplayName(ByVal fieldAccessors() As String) As String Implements IDisplayNameProvider.GetFieldDisplayName
-			' Get a field name form the data member's name. 
-			Dim fieldName As String = fieldAccessors(fieldAccessors.Length - 1)
+        Private Function GetFieldDisplayName(ByVal fieldAccessors As String()) As String Implements IDisplayNameProvider.GetFieldDisplayName
+            ' Get a field name form the data member's name. 
+            Dim fieldName As String = fieldAccessors(fieldAccessors.Length - 1)
+            ' Hide the data member if its name ends with 'ID'.
+            If fieldName.EndsWith("ID") Then
+                Return Nothing
+            End If
 
-			' Hide the data member if its name ends with 'ID'.
-			If fieldName.EndsWith("ID") Then
-				Return Nothing
-			End If
+            ' Hide the 'Products' table, because its fields are accessible 
+            ' via the 'CategoriesProducts' relation only.
+            If fieldAccessors(0).StartsWith("Products") Then
+                Return Nothing
+            End If
 
-			' Hide the 'Products' table, because its fields are accessible 
-			' via the 'CategoriesProducts' relation only.
-			If fieldAccessors(0).StartsWith("Products") Then
-				Return Nothing
-			End If
+            ' Insert spaces between separate words of a field name.
+            Return ChangeNames(fieldName)
+        End Function
 
-			' Insert spaces between separate words of a field name.
-			Return ChangeNames(fieldName)
-		End Function
-		Public Function ChangeNames(ByVal name As String) As String
-			Dim result As String = String.Empty
-			Dim isPrevLow As Boolean = False
+        Public Function ChangeNames(ByVal name As String) As String
+            Dim result As String = String.Empty
+            Dim isPrevLow As Boolean = False
+            For Each symb As Char In name
+                ' Check if a character is of upper case.
+                ' To avoid spaces inside abbreviations, 
+                ' check if the previous character is of upper case, too.
+                If Char.IsUpper(symb) AndAlso isPrevLow Then
+                    result += " " & symb
+                Else
+                    result += symb
+                End If
 
-			For Each symb As Char In name
-				' Check if a character is of upper case.
-				' To avoid spaces inside abbreviations, 
-				' check if the previous character is of upper case, too.
-				If Char.IsUpper(symb) AndAlso isPrevLow Then
-					result &= " " & symb
-				Else
-					result &= symb
-				End If
-				isPrevLow = Char.IsLower(symb)
-			Next symb
-			Return result
-		End Function
-		' ...
-	End Class
-	#End Region ' #SqlDataSource
+                isPrevLow = Char.IsLower(symb)
+            Next
+
+            Return result
+        End Function
+    ' ...
+    End Class
+'#End Region  ' #SqlDataSource
 End Namespace
